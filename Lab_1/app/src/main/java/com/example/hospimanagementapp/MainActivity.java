ff48cf26e166a86f6b78233d787766beb588da11
James-W-LTU <j.woodthorpe03@gmail.com>
2025-10-24 11:20:56 +0100

    edits

--- Lab_1/app/src/main/java/com/example/hospimanagementapp/MainActivity.java
+++ Lab_1/app/src/main/java/com/example/hospimanagementapp/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,3 +24,5 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+
+
