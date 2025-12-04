5f1d961bba3df0d2188638a6c09f75e0a770ab7d
James-W-LTU <j.woodthorpe03@gmail.com>
2025-12-04 18:16:03 +0000

    Lab 2 Progress

--- Lab_1/app/src/main/java/com/example/hospimanagementapp/AdminPortalActivity.java
+++ Lab_1/app/src/main/java/com/example/hospimanagementapp/AdminPortalActivity.java
@@ -67,12 +67,4 @@ public class AdminPortalActivity extends AppCompatActivity {
             }
         });
     }
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//
-//        setContentView(R.layout.activity_admin_portal);
-//
-//    }
 }
